d82413306e3aa60031915b2b3e0fed9ba1b6219a
Or <orsaa@post.bgu.ac.il>
2018-11-20 18:08:06 +0200

    table created
    table creation and other fix code

--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,7 +1,7 @@
 import * as esprima from 'esprima';
 
 const parseCode = (codeToParse) => {
-    return esprima.parseScript(codeToParse,{loc:true});
+    return esprima.parseScript(codeToParse,{loc: true});
 };
 
 export {parseCode};
@@ -11,6 +11,7 @@ let statements = [];
 const func = {
     'FunctionDeclaration': function_handle,
     'VariableDeclaration': variable_handle,
+    'Identifier': identifier_handle,
     'ExpressionStatement': expression_handle,
     'WhileStatement': while_handle,
     'IfStatement': if_handle,
@@ -18,17 +19,22 @@ const func = {
     'ForStatement': for_handle,
     'BlockStatement': body_iter,
     'AssignmentExpression': assignment_handle,
-    //'UnaryExpression': unary_handle,
     'BinaryExpression': binary_handle,
     'UpdateExpression': update_handle,
     'MemberExpression': member_handle,
-    'Identifier': identifier_handle,
+    'Literal': literal_handle,
+    'UnaryExpression': unary_handle
     //  'VariableDeclarator': var_dec_handle
-    'Literal': literal_handle
 };
 
+const keys = ['Line','Type','Name','Condition', 'Value'];
+//const fields = ['line','type','name','condition','value'];
+
+function identifier_handle(identifier){
+    return identifier.name;
+}
+
 function create_objects(parseCode) {
-    window.alert('a');
     if (parseCode.type.valueOf() === 'Program') {
         program_handle(parseCode);
     }
@@ -39,23 +45,20 @@ function create_objects(parseCode) {
 export {create_objects};
 
 function program_handle(program) {
-    window.alert('aa');
-    body_iter(program.body);
+    body_iter(program);
 }
 
-function function_handle(func) {
-    let func_name = func[func.id.type](func.id);
-    statements.push({line: func.loc.start.line, type: func.type, name: func_name, condition: undefined, value: undefined});
-    for (let i =0; i<func.params.length;++i) {
-        window.alert('aaa');
-        let param_name = func[func.params[i].type](func.params[i]);
-        statements.push({line: func.params[i].loc.start.line, type: func.params[i].type, name: param_name, condition: undefined, value: undefined});
+function function_handle(exp) {
+    let func_name = func[exp.id.type](exp.id);
+    statements.push({line: exp.id.loc.start.line, type: exp.type, name: func_name, condition: undefined, value: undefined});
+    for (let i =0; i<exp.params.length;i++) {
+        let param_name = func[exp.params[i].type](exp.params[i]);
+        statements.push({line: exp.params[i].loc.start.line, type: exp.params[i].type, name: param_name, condition: undefined, value: undefined});
     }
-    func[func.body.type](func.body);
+    func[exp.body.type](exp.body);
 }
 
 function variable_handle(obj) {
-    window.alert('b');
     for (let i=0; i<obj.declarations.length; ++i) {
         let dec = obj.declarations[i];
         //let name = expressions[obj.declarations[i].type](obj.declarations[i]);
@@ -82,7 +85,6 @@ function return_handle(statement){
 }
 
 function body_iter(statement){
-    window.alert('b');
     let body = statement.body;
     for(let i=0;i<body.length;++i){
         func[body[i].type](body[i]);
@@ -90,9 +92,8 @@ function body_iter(statement){
 }
 
 function if_handle(exp){
-    window.alert('b');
     let condition = func[exp.test.type](exp.test);
-    statements.push({line: exp.loc.start.line, type: exp.type, name: exp.left.name, condition: condition, value: exp.right.value});
+    statements.push({line: exp.loc.start.line, type: exp.type, name: undefined, condition: condition, value: undefined});
     func[exp.consequent.type](exp.consequent);
     func[exp.alternate.type](exp.alternate);
 }
@@ -109,11 +110,6 @@ function for_handle(statement){
 }
 
 function binary_handle(exp){
-    // let one,two,three,four;
-    // one = ''; two = ''; three = ''; four = '';
-    // if(exp.left.type === 'Binart'){
-    //
-    // }
     let left = func[exp.left.type](exp.left);
     let right = func[exp.right.type](exp.right);
     let operator = exp.operator;
@@ -124,10 +120,6 @@ function update_handle(exp){
     let name = func[exp.argument.type](exp.argument);
     let operator = exp.operator;
     statements.push({line: exp.loc.start.line, type: exp.type, name: name, condition: undefined, value: operator+name});
-}
-
-function identifier_handle(identifier){
-    return identifier.name;
 }
 
 function literal_handle(literal){
@@ -142,4 +134,45 @@ function member_handle(exp) {
 
 function expression_handle(exp){
     func[exp.expression.type](exp.expression);
+}
+
+function unary_handle(exp){
+    let argu = func[exp.argument.type](exp.argument);
+    return exp.operator+argu;
+}
+
+function table_create(){
+    let body = document.getElementsByTagName('body')[0];
+    let tbl  = document.createElement('myTable');
+    tbl.style.width  = '100px';
+    tbl.style.border = '1px solid black';
+    let header = tbl.createTHead();
+    let row = header.insertRow(0);
+    // insert columns
+    for (let i = 0, l = keys.length; i < l; i ++) {
+        let cell = row.insertCell(i);
+        // cell.innerHTML = '<b>'+keys[i]+'</b>';
+        cell.innerHTML = keys[i];
+    }
+    add_rows();
+    body.appendChild(tbl);
+}
+
+export {table_create};
+
+function add_rows() {
+    let table = document.getElementById('myTable');
+    for (let i = 0; i < statements.length; i++) {
+        let newRow = table.insertRow(table.length);
+        let j = 0;
+        for (let x in statements[i]) {
+            let cell = newRow.insertCell(j);
+            if(statements[i].hasOwnProperty(x)){
+                cell.innerHTML = statements[i][x];
+            }
+            else
+                cell.innerHTML = 'a';
+        }
+    }
+    document.getElementById('myTable').style.visibility = 'visible';
 }
