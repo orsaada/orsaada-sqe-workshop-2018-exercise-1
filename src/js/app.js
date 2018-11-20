9980f8f9ef1b9fac9f951a84262eca333d9f42c3
Or <orsaa@post.bgu.ac.il>
2018-11-20 18:28:25 +0200

    run app

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,16 @@
 import $ from 'jquery';
 import {parseCode} from './code-analyzer';
-
 import {create_objects} from './code-analyzer';
+import {table_create} from './code-analyzer';
+import {statements} from './code-analyzer';
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
         create_objects(parsedCode);
+        table_create();
+        //console.log(`is8 ${JSON.stringify(statements)}`);
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
     });
 });
